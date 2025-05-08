b1e520e97de0d3451e3301ae017dfbf71ec13bb7
aashtiany <amir.klavuhn-ashtiany@dodiis.mil>
2025-05-08 11:57:14 -0400

    adding health check

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/workouts", workoutRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.get("api/health", (req, res) => {
+
+app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "Backend is live!" });
 });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
